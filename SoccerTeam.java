5a4a50fd6b53815e777916f20ed6efb94e520e15
richardcarlson3 <richardcarlson@billericak12.com>
2024-02-08 10:33:48 -0500

    hi

---
+++ SoccerTeam.java
@@ -0,0 +1,12 @@
+public class SoccerTeam {
+
+    private int wins;
+    private int losses;
+    private int ties;
+
+    public SoccerTeam(){
+        wins=0;
+        losses=0;
+        ties=0;
+    }
+}
