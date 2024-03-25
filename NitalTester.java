d2fca0ee14812cc681db837659607bd4bd1db893
richardcarlson3 <richardcarlson@billericak12.com>
2024-03-25 10:23:04 -0400

    hi

--- NitalTester.java
+++ NitalTester.java
@@ -1,12 +1,15 @@
-import java.util.Arrays;
+    import java.util.Arrays;
+
 
 public class NitalTester {
+
 
     /** Returns true if and only if every value in arr1 appears in arr2.
      * Precondition: arr1 and arr2 are the same length.
      * Postcondition: arr1 and arr2 are unchanged.
      */
     public static boolean hasAllValues(int [] arr1, int[] arr2){
+
 
         for(int i = 0; i < arr1.length; i++){
             boolean found = false;
@@ -17,6 +20,7 @@ public class NitalTester {
         }
         return true;
     }
+
 
     /** Returns true if arr contains any repeated values, false otherwise.
      */
@@ -29,7 +33,8 @@ public class NitalTester {
         return false;
     }
 
-    
+
+   
     /* Write a static method getColumn, which returns a one-dimensional
     array containing the elements of a single column in a two-dimensional
     array.  The elements in the returned array should be in the same order
@@ -37,16 +42,20 @@ public class NitalTester {
     the array element at row r and column c
     The following code segment initializes an array and calls getColumn().
 
+
     int[][] arr2D = { { 0, 1, 2},
                       { 3, 4, 5},
                       { 9, 5, 3}};
     int[] result = NitalTester.getColumn(arr2D, 1);
 
+
     When the code segment has completed execution, the variable result
     will have the following contents:
 
+
                 result: {1, 4, 5}
      */
+
 
     /** Returns an array containing the elements of column c of
      * arr2D in the same order they appear in arr2D
@@ -54,13 +63,15 @@ public class NitalTester {
      * Postcondition: arr2D is unchanged.
      */
     public static int [] getColumn(int[][] arr2D, int c){
-		int[] a=new int[arr2D.length];
-        for(int i=0; i<arr2D.length; i++){
-            a[i]=arr2D[i][c];
+        int [] result=new int[arr2D[0].length];
+
+
+        for(int i=0;i<arr2D[0].length;i++){
+            result[i]=arr2D[i][c];
         }
-		
-		return a;  //replace this
+        return result;
     }
+
 
     /* Write the method isNital below.  You must use getColumn,
     hasAllValues and containsRepeats appropriately to receive
@@ -72,33 +83,36 @@ public class NitalTester {
      *      square has at least one row.
      */
     public static boolean isNital(int[][] square){
-		
-		
-        
-		
-		
-        return false;   // replace this
+        if(containsRepeats(square[0])==true) return false;
+        for(int i=0;i<square.length;i++){
+            if(hasAllValues(square[0],square[i])){
+                if(hasAllValues(square[0], getColumn(square,i))){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
+
     public static void main(String[] args){
-      int[][] n1 = {{1,2,3},{2,3,1},{3,1,2}};
+        int[][] n1 = {{1,2,3},{2,3,1},{3,1,2}};
         int[][] n2 = {{10,30,20,0},{0,20,30,10},{30,0,10,20},{20,10,0,30}};
         int[][] n3 = {{1,2,1},{2,1,1},{1,1,2}};
         int[][] n4 = {{1,2,3},{3,1,2},{7,8,9}};
         int[][] n5 = {{1,2},{1,2}};
-	int[][] n6 = {{1,1},{2,2}};
+    int[][] n6 = {{1,1},{2,2}};
+
 
         System.out.println(Arrays.toString(getColumn(n1, 2)));
         System.out.println(Arrays.toString(getColumn(n4, 0)));
+
 
         System.out.println(isNital(n1));
         System.out.println(isNital(n2));
         System.out.println(isNital(n3));
         System.out.println(isNital(n4));
         System.out.println(isNital(n5));
-	System.out.println(isNital(n6));
-		
-
-
+        System.out.println(isNital(n6));
     }
 }
